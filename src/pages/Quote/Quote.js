6690039ce9a715c340d5e15954fa9d2101977193
GeorgeCheng1023 <ck40292@gmail.com>
2022-05-08 13:45:24 +0800

    deploy

--- src/pages/Quote/Quote.js
+++ src/pages/Quote/Quote.js
@@ -1,16 +1,26 @@
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import QuoteDetail from './QuoteDetail'
-import NewQuotes from './NewQuotes'  
-import AllQuotes from './AllQuotes'
+import LoadingSpinner from "../../components/UI/LoadingSpinner";
+const AllQuotes = React.lazy(() => import("./AllQuotes"));
+const QuoteDetail = React.lazy(() => import("./QuoteDetail"));
+const NewQuotes = React.lazy(() => import("./NewQuotes"));
 
-const Quote  = () => {
+const Quote = () => {
   return (
-    <Routes>
-      <Route path='/' element={<AllQuotes/>}/>
-      <Route path=":quoteID/*" element={<QuoteDetail/>}/>
-      <Route path="new" element={<NewQuotes />}/>
-    </Routes>
-  )
-}
+    <Suspense
+      fallback={
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<AllQuotes />} />
+        <Route path=":quoteID/*" element={<QuoteDetail />} />
+        <Route path="new" element={<NewQuotes />} />
+      </Routes>
+    </Suspense>
+  );
+};
 
-export default Quote
+export default Quote;
