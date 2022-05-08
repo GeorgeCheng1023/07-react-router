208cb4e2e699b19d6fdbcf6308a89ff7cb0c25b8
GeorgeCheng1023 <ck40292@gmail.com>
2022-05-08 13:51:10 +0800

    fix(index): url root

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<BrowserRouter><App/></BrowserRouter>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/07-react-router/*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
